f96a899e24e49e7605dbadc51647980b52ec3082
fracara <caraballo.h.frank@gmail.com>
2021-10-09 17:29:22 +0900

    handle server error

--- server/index.py
+++ server/index.py
@@ -4,16 +4,22 @@ from proto_build import blog_pb2_grpc
 from blog import BlogPostUseCase
 
 def server() -> None:
-    # Create Grpc Server
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    try:
+        # Create Grpc Server
 
-    # Add Implementation for BlogPostService
-    blog_pb2_grpc.add_BlogPostServiceServicer_to_server(servicer=BlogPostUseCase(), server=server)
-    
-    # Intitialize and start the server
-    server.add_insecure_port('[::]:50051')
-    server.start()
-    print('Listening on Port 50051')
-    server.wait_for_termination()
+        # Add Implementation for BlogPostService
+        blog_pb2_grpc.add_BlogPostServiceServicer_to_server(
+                servicer=BlogPostUseCase(), 
+                server=server
+            )
+        
+        # Intitialize and start the server
+        server.add_insecure_port('[::]:50051')
+        server.start()
+        print('Listening on Port 50051')
+        server.wait_for_termination()
+    except:
+        server.stop(grace=None)
 
 server()
